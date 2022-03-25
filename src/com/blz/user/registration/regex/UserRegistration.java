b185d28e78276c840ea6754f67a988d49888132e
TejShind <tej.gholap25@gmail.com>
2022-03-25 08:13:07 +0530

    [Tejaswini]ADDED: Modified Regex pattern should have exact one special character

--- src/com/blz/user/registration/regex/UserRegistration.java
+++ src/com/blz/user/registration/regex/UserRegistration.java
@@ -9,7 +9,10 @@ public class UserRegistration {
     public final String lastName = ("^[A-Z]{1}[a-z]{3,}$");
     public final String email = "^[a-z]{3,}[1-9]{0,4}[@][a-z]{4,}[.][a-z]{2,3}$";
     public final String mobileNumber = "^[0-9]{2}\s?[0-9]{10}$";//e.g 91 9919819801.
-    public final String passWord = "^[A-Za-z]{0,}[@]{1}[a-zA-Z1-9]{1}[a-zA-Z0-9]{7,}$";//Exactly one special character,minimum 8 characters,One uppercase character, One Numeric number
+    //    public final String passWord = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*])[a-zA-Z1-9]{1}[a-zA-Z0-9]{7,}";
+    public final String passWord = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*():<>?]).{8,20}";
+
+    // public final String passWord = "^[A-Za-z]{0,}[a-zA-Z1-9]{1}[@]{1}[a-zA-Z0-9]{7,}$";//Exactly one special character,minimum 8 characters,One uppercase character, One Numeric number
     public void validateFirstName(String enteredFirstName) {
         Pattern pattern = Pattern.compile(firstName);
         Matcher matcher = pattern.matcher(enteredFirstName);
